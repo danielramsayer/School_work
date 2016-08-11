b4ade24716aed2f5a6f844c4539376fef0804546
schoenertraeumer <schoentraumer@gmail.com>
2016-08-11 13:48:14 -0700

    Create Pyg.py

---
+++ random-programs/Pyg.py
@@ -0,0 +1,27 @@
+"""
+This is a quick pig latin translater to test python code
+
+Daniel Ramsayer
+4/30/2016
+"""
+
+#The 'latin'
+pyg = 'ay'
+
+original = raw_input('Text to be translated into PygLatin, please: ')
+
+string1 = []
+string2 = []
+
+#Tranlate input into pyglatin.
+string1 = original.split()
+for word in string1:
+    #convert to lower case
+    lword = word.lower()
+    #grab the first letter
+    first = lword[0]
+    #create the 'latin word'
+    new_word = lword[1:len(lword)] + first + pyg
+    #print new_word
+    string2.append(new_word)
+print ' '.join(string2)
