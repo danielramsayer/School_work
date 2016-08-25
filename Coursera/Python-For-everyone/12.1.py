c1f3169dc584843d89f038398f9c038df7cbb56a
schoenertraeumer <schoentraumer@gmail.com>
2016-08-25 11:08:55 -0700

    Update 12.1.py
    Made some updates, the original version I could not get to pass error checking.

--- Coursera/Python-For-everyone/12.1.py
+++ Coursera/Python-For-everyone/12.1.py
@@ -1,23 +1,21 @@
 import socket
-import urllib2
+import urllib
 
 website = raw_input("Enter website: ")
-splitweb = website.split("/")
-url = splitweb[2]
-
+url = website.split("/")[2]
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect((url, 80))
 mysock.send('GET ' + website + ' HTTP/1.0\n\n')
 
-try:
-    while seconddatastream.status == 200:
-        data = mysock.recv(512)
-        if ( len(data) < 1 ) :
-            break
-        print data;
-except:
+valid = urllib.urlopen(website).getcode()
+
+while valid == 200:
+    data = mysock.recv(512)
+    if ( len(data) < 1 ):
+        break
+    print data
+else:
     print "I'm sorry, that website does not appear to be valid. Please try again."
-    
 
 mysock.close()
