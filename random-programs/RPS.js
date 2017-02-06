a622abbcc38a86b206f4a5795bc00b27f64ee2e9
schoenertraeumer <schoentraumer@gmail.com>
2017-02-06 15:35:40 -0800

    Update RPS.js

--- random-programs/RPS.js
+++ random-programs/RPS.js
@@ -3,43 +3,47 @@ var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 
 if (computerChoice < 0.34) {
-	computerChoice = "rock";
+   computerChoice = "rock";
 } else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
+   computerChoice = "paper";
 } else {
-	computerChoice = "scissors";
-} 
+   computerChoice = "scissors";
+}
 
-var compare = function (choice1, choice2) {{
-if (choice1 === choice2) {
+var compare = function (choice1, choice2) {
+var choice1 = choice1.toLowerCase();
+if ( choice1 === choice2) {
     return "The result is a tie!";
 } 
-else if (choice1 === "rock") { 
+else if (choice1 === "rock") 
+    { 
     if(choice2 === "scissors") {
         return "Computer chose Scissors - Rock wins!";
+    }    
+    else if(choice2 === "paper") {
+        return "Computer Chose Paper - Paper Wins!";
     }
-    else {
-        return "Computer chose Paper - Paper Wins!";
-    }
-}}
-{
-if (choice1 === "paper") { 
+}
+else if (choice1 === "paper") 
+    { 
     if(choice2 === "scissors") {
         return "Computer chose scissors - scissors wins!";
     }
-    else {
+    else if(choice2 === "rock") {
         return "Computer Chose Rock - Paper Wins!";
     }
-}}
-{
-if (choice1 === "scissors") { 
+}
+else if (choice1 === "scissors") 
+    { 
     if(choice2 === "paper") {
         return "Computer chose paper - scissors wins!";
     }
-    else {
-        return "Computer Chose Rock - Rock Wins!";
+    else if (choice2 === "rock") {
+        return "Computer Chose Rock - Scissors Wins!";
     }
-}}  
 }
-
+else {
+        return "That is not a valid response, please try again.";
+    }
+}
 compare(userChoice, computerChoice)
