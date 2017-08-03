1a1eea57b1fd01502f2d7581a77a43980b27812c
Daniel Ramsayer <danielramsayer@Daniels-MacBook-Pro.local>
2017-08-02 19:18:11 -0700

    Minor updates to button pressing proceedure.

--- FreeCodeCamp/AdvancedPrograms/calculator/main.js
+++ FreeCodeCamp/AdvancedPrograms/calculator/main.js
@@ -1,8 +1,23 @@
 var show = "0";
 var total = "0";
+var current = [];
 
 document.getElementById('total').innerHTML = total;
 
-function eachTime() {
+function buttonPressed(numbButt) {
+  switch (numbButt){
+    case 'CE':
+      total = 0;
+      return total;
+    case 'C':
+      current = 0;
+      show = 0;
+    case "<-":
+      current = current.toString().slice(0,-1);
+      show = current;
+    case "0":
+      if (current[0] === "0") {
 
+      }
+  }
 }
