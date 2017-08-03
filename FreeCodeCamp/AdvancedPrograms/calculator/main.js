66acdcb2c4a7e18f593da2654d8cfd69a2eb7451
Daniel Ramsayer <danielramsayer@Daniels-MacBook-Pro.local>
2017-08-02 21:02:47 -0700

    Getting there!

--- FreeCodeCamp/AdvancedPrograms/calculator/main.js
+++ FreeCodeCamp/AdvancedPrograms/calculator/main.js
@@ -1,23 +1,41 @@
-var show = "0";
-var total = "0";
-var current = [];
+var current = [].join(',');
+var old = [];
 
-document.getElementById('total').innerHTML = total;
+var firstShow = document.getElementById('total');
+firstShow.innerHTML = 0;
 
 function buttonPressed(numbButt) {
-  switch (numbButt){
+  switch (numbButt) {
     case 'CE':
-      total = 0;
-      return total;
+      total = "0";
     case 'C':
-      current = 0;
-      show = 0;
-    case "<-":
-      current = current.toString().slice(0,-1);
-      show = current;
+      current = "0";
+      break;
+    case "back":
+      current = current.toString().slice(0, -1);
+      break;
     case "0":
-      if (current[0] === "0") {
-
+      if (current.length < 1 && current[0] !== "0") {
+        current = current.concat("0");
       }
+      break;
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      if (current === '0') {
+        current = "";
+      }
+      current = current.concat(numbButt);
+      break;
+    default:
+
   }
+  var show = current.toString();
+  firstShow.innerHTML = show;
 }
