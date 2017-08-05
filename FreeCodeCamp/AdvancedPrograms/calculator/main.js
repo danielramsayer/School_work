1bdcc3194a784712f93692a6aa26102ebc94ae6d
Daniel Ramsayer <danielramsayer@Daniels-MacBook-Pro.local>
2017-08-04 21:12:24 -0700

    Fixed and DONE! Also A+ Atom.

--- FreeCodeCamp/AdvancedPrograms/calculator/main.js
+++ FreeCodeCamp/AdvancedPrograms/calculator/main.js
@@ -7,63 +7,62 @@ firstShow.innerHTML = 0;
 function buttonPressed(numbButt) {
   switch (numbButt) {
     case 'CE':
-      current = "0";
+      current = [];
+      current = '0';
+      old = [];
       break;
     case 'C':
-      current = "0";
+      current = '0';
       break;
-    case "back":
+    case 'back':
       current = current.toString().slice(0, -1);
       break;
-    case "0":
+    case '0':
       if (current[0] === '0' && current[1] === '.' || current[0] !== '0') {
-        current = current.concat("0");
+        current = current.concat('0');
       }
       break;
-    case "1":
-    case "2":
-    case "3":
-    case "4":
-    case "5":
-    case "6":
-    case "7":
-    case "8":
-    case "9":
-      current = current.concat(numbButt);
-      if (current[0] === '0' && current[1] !== '.') {
-        var shifted = current.shift();
-      }
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      current = current.concat(numbButt).replace(/^0+/,'');
       break;
-    case ".":
+    case '.':
       if (current.includes('.') === false) {
         current = current.concat(numbButt);
       }
       break;
-    case "times":
+    case 'times':
       old = old.concat(current, '*');
       current = [].join(',');
-      var show = current[-1].toString();
+      show = current[-1].toString();
       firstShow.innerHTML = show;
       break;
-    case "divide":
+    case 'divide':
       old = old.concat(current, '/');
       current = [].join(',');
-      var show = current[-1].toString();
+      show = current[-1].toString();
       firstShow.innerHTML = show;
       break;
-    case "minus":
+    case 'minus':
       old = old.concat(current, '-');
       current = [].join(',');
-      var show = current[-1].toString();
+      show = current[-1].toString();
       firstShow.innerHTML = show;
       break;
-    case "plus":
+    case 'plus':
       old = old.concat(current, '+');
       current = [].join(',');
-      var show = current[-1].toString();
+      show = current[-1].toString();
       firstShow.innerHTML = show;
       break;
-    case "equals":
+    case 'equals':
       old = old.concat(current);
       total = old.join(' ');
       total = eval(total);
