ab4c27209e8c00825096d9a9e0a41610f48864f1
Daniel Ramsayer <danielramsayer@Daniels-MacBook-Pro.local>
2017-08-10 14:46:16 -0700

    Update.

--- FreeCodeCamp/AdvancedPrograms/TicTacToe/main.js
+++ FreeCodeCamp/AdvancedPrograms/TicTacToe/main.js
@@ -1,7 +1,9 @@
-document.getElementById("grid").onload = function() {getChar()};
-
 var score1 = 0;
 var score2 = 0;
+var playerVariable = "";
+var winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+var turnCount = 0;
+var used = [];
 
 var pnScores1 = document.getElementById('PnScores1');
 pnScores1.innerHTML = score1;
@@ -9,6 +11,28 @@ pnScores1.innerHTML = score1;
 var pnScores2 = document.getElementById('PnScores2');
 pnScores2.innerHTML = score2;
 
-function getChar() {
-  playerVariable = prompt("Please select either 'X' or 'O' for your player token.")
+function thoseClicked (clickedSquare) {
+  switch (clickedSquare) {
+    case 'a0':
+      if (used.includes[0]) {
+        break;
+      } else {
+        used.append('0');
+        var pnScores2 = document.getElementById('PnScores2');
+        pnScores2.innerHTML = score2;
+      }
+  }
+
+}
+
+/*function checker() {
+  playerVariable
+}*/
+
+function keepScore (winner) {
+  if (winner === "player1") {
+    score1 += 1;
+  } else if (winner === "player2"){
+    score2 += 1;
+  } //if winner is neither of them, neither get the point.
 }
---
+++ random-programs/squares.js
@@ -0,0 +1,22 @@
+function showPattern(){
+  var top = 25, left = 25;
+  var width = 300, height=300;
+  var colourList = ["red","orange","yellow","green","blue","purple"];
+  var the_body = document.getElementById("theBody");
+
+  while (width > 50) {
+    var thisDiv = document.createElement("div");
+    var randomColour = Math.random() * 7;
+    randomColour = Math.floor(randomColour);
+    thisDiv.style.top = top + "px";
+    thisDiv.style.left = left + "px";
+    thisDiv.style.width = width + "px";
+    thisDiv.style.height = height + "px";
+    thisDiv.style.background = colourList(randomColour);
+    theBody.appendChild(thisDiv);
+    top += 15;
+    left = 15;
+    width -= 10;
+    height -= 10;
+  }
+}
