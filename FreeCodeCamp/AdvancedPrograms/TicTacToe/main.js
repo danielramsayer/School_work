83c044a07bd49cf0b4110e9ed15bcb1216f2baca
Daniel Ramsayer <danielramsayer@Daniels-MacBook-Pro.local>
2017-08-23 17:36:39 -0700

    Ran into an error with the winchecker function, reevaluating.

--- FreeCodeCamp/AdvancedPrograms/TicTacToe/main.js
+++ FreeCodeCamp/AdvancedPrograms/TicTacToe/main.js
@@ -22,10 +22,12 @@ function thoseClicked(clickedSquare) {
       element = document.getElementById(clickedSquare);
       element.innerHTML = playerVariable;
       winCheck(pused);
+      console.log(turnCount);
     } else {
       alert("Already in use. Try another!");
     }
     turnCount++;
+    //aI();
   } else {
     alert("This is a tie, no one wins.");
     reset();
@@ -33,45 +35,34 @@ function thoseClicked(clickedSquare) {
 }
 
 function aI() {
-  
-
+  if (turnCount === 1) {
+    if (pused.includes(0) || pused.includes(2) || pused.includes(6) || pused.includes(8) || pused.includes(1) || pused.includes(3) || pused.includes(5) || pused.includes(7)) {
+      celement = document.getElementById('a4');
+      celement.innerHTML = computerVariable;
+    } else {
+      celement = document.getElementById('a0');
+      celement.innerHTML = computerVariable;
+    }
+  }
 }
 
 
 function winCheck(array1) {
-  array1 = array1.sort();
-  if (array1.includes(0)) {
-    if (array1.includes(1) && array1.includes(2)) {
-      won(0);
-    } else if (array1.includes(3) && array1.includes(6)){
-      won(0);
-    } else if (array1.includes(4) && array1.includes(8)){
-      won(0);
-    }
-  } else if (array1.includes(1)) {
-    if (array1.includes(4) && array1.includes(7)) {
-      won(1);
-    }
-  } else if (array1.includes(2)) {
-    if (array1.includes(4) && array1.includes(6)) {
-      won(2);
-    } else if (array1.includes(5) && array1.includes(8)){
-      won(2);
-    }
-  } else if (array1.includes(3)) {
-    if (array1.includes(4) && array1.includes(5)) {
-      won(3);
-    }
-  } else if (array1.includes(6)) {
-    if (array1.includes(7) && array1.includes(8)) {
-      won(6);
-    }
+  if (
+    (array1.includes(0) && array1.includes(1) && array1.includes(2)) || (array1.includes(0) && array1.includes(3) && array1.includes(6)) ||
+    (array1.includes(0) && array1.includes(4) && array1.includes(8)) ||
+    (array1.includes(1) && array1.includes(4) && array1.includes(7)) ||
+    (array1.includes(2) && array1.includes(4) && array1.includes(6)) || (array1.includes(2) && array1.includes(5) && array1.includes(8)) ||
+    (array1.includes(3) && array1.includes(4) && array1.includes(5)) ||
+    (array1.includes(6) && array1.includes(7) && array1.includes(8))
+  ) {
+    return 
   }
 }
 
 function won(checker) {
   alert("Winning Move!");
-  if (pused.includes(checker)){
+  if (pused.includes(checker)) {
     keepScore('player1');
   } else {
     keepScore('player2');
@@ -91,15 +82,15 @@ function keepScore(winner) {
 function reset() {
   for (var i = 0; i < 9; i++) {
     document.getElementById('a' + i).innerHTML = "";
-    turnCount = 0;
     used = [];
     pused = [];
     cused = [];
+    turnCount = 0;
   }
 
-var pnScores1 = document.getElementById('PnScores1');
-pnScores1.innerHTML = score1;
+  var pnScores1 = document.getElementById('PnScores1');
+  pnScores1.innerHTML = score1;
 
-var pnScores2 = document.getElementById('PnScores2');
-pnScores2.innerHTML = score2;
+  var pnScores2 = document.getElementById('PnScores2');
+  pnScores2.innerHTML = score2;
 }
