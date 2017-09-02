fc3437cd44134653839d5b9edf0fda734853bfcb
Daniel Ramsayer <danielramsayer@Daniels-MacBook-Pro.local>
2017-09-01 21:13:11 -0700

    Completed Assignment.

--- FreeCodeCamp/AdvancedPrograms/TicTacToe/main.js
+++ FreeCodeCamp/AdvancedPrograms/TicTacToe/main.js
@@ -1,140 +1,175 @@
 var score1 = 0;
 var score2 = 0;
-var playerVariable = "X";
-var computerVariable = "O";
-var turnCount = 0;
-var whosTurn = false;
-var used = [];
-var pused = [];
-var cused = [];
+var playerVariable = 'X';
+var computerVariable = 'O';
 
 var pnScores1 = document.getElementById('PnScores1');
 pnScores1.innerHTML = score1;
-
 var pnScores2 = document.getElementById('PnScores2');
 pnScores2.innerHTML = score2;
 
-function winCheck(player, array) {
-  if (
-    (array.includes(0) && array.includes(1) && array.includes(2)) ||
-    (array.includes(0) && array.includes(3) && array.includes(6)) ||
-    (array.includes(0) && array.includes(4) && array.includes(8)) ||
-    (array.includes(1) && array.includes(4) && array.includes(7)) ||
-    (array.includes(2) && array.includes(4) && array.includes(6)) ||
-    (array.includes(2) && array.includes(5) && array.includes(8)) ||
-    (array.includes(3) && array.includes(4) && array.includes(5)) ||
-    (array.includes(6) && array.includes(7) && array.includes(8))
-  ) {
-    won(player);
-    return true;
-  } else {
-    return false;
-  }
-}
+$(document).ready(function() {
+  var xORo = confirm("Press 'OK' for X! Press 'Cancel' for O!");
 
-function thoseClicked(clickedSquare) {
-  if (turnCount < 9) {
-    whosTurn = false;
-    n = parseInt(clickedSquare.slice(1), 10);
-    if (!(used.includes(n))) {
-      used.push(n);
-      pused.push(n);
-      element = document.getElementById(clickedSquare);
-      element.innerHTML = playerVariable;
-      winCheck('Player', pused);
-      console.log(turnCount);
+  if (xORo === false) {
+    computerVariable = 'X';
+    playerVariable = 'O';
+  }
+
+  var board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+  ];
+
+  $('.marker-select').click(function () {
+    while (!board.includes("O") || !board.includes("X")) {
+
+    }
+  });
+
+  $('.square').click(function() {
+    var stateOfGame = winCheck(board);
+    if (stateOfGame || stateOfGame === null) {
+      return;
+    }
+    $this = $(this);
+    var i = $this.data('i');
+    var j = $this.data('j');
+    if (board[i][j] !== " ") {
+      alert("This is already occupied, please try again.");
+      return;
+    }
+    $this.html(playerVariable);
+    board[i][j] = playerVariable;
+    stateOfGame = winCheck(board);
+    if (stateOfGame) {
+      score1 += 1;
+      alert("Player wins! Good Job!");
+      reset();
+      return;
+    } else if (stateOfGame === null) {
+      alert("Tie! Good luck next time.");
+      reset();
+      return;
     } else {
-      alert("Already in use. Try another!");
+      var move = aiTurn();
+      board[move.i][move.j] = computerVariable;
+      $('.square[data-i=' + move.i + '][data-j=' + move.j + ']').html(computerVariable);
     }
-    turnCount++;
-    aIminmax();
-  } else {
-    alert("This is a tie, no one wins.");
-    reset();
-  }
-}
-
-function won(player) {
-  if (whosTurn === false) {
-    keepScore('player1');
-  } else {
-    keepScore('player2');
-  }
-  reset();
-}
-
-function keepScore(winner) {
-  if (winner === "player1") {
-    score1 += 1;
-  } else if (winner === "player2") {
-    score2 += 1;
-  } //if winner is neither of them, neither get the point.
-}
-
-function aIminmax() {
-  var k;
-  console.log(turnCount);
-  if (turnCount === 1) {
-    if (used.includes(0) || used.includes(2) || used.includes(6) || used.includes(8) || used.includes(1) || used.includes(3) || used.includes(5) || used.includes(7)) {
-      k = 4;
-      used.push(4);
-      cused.push(4);
-      $("#a" + 4).text(computerVariable);
-    } else {
-      k = 0;
-      used.push(k);
-      cused.push(k);
-      $("#a" + k).text(computerVariable);
-    }
-    turnCount++;
-  } else if (turnCount === 3) {
-    if (pused) {
-
-    }
-  }
-}
-
-function reset() {
-  for (var i = 0; i < 9; i++) {
-    document.getElementById('a' + i).innerHTML = "";
-    used = [];
-    pused = [];
-    cused = [];
-    turnCount = 0;
-  }
-
-  var pnScores1 = document.getElementById('PnScores1');
-  pnScores1.innerHTML = score1;
-
-  var pnScores2 = document.getElementById('PnScores2');
-  pnScores2.innerHTML = score2;
-}
-
-
-function aiturn(board, depth, player, turn) {
-  if (checkWin(board, !player))
-    return -10 + depth;
-  if (checkFull(board))
-    return 0;
-  var value = player == HUMAN ? HUMVAL : COMVAL;
-  var max = -Infinity;
-  var index = 0;
-  for (var x = 0; x < 9; x++) {
-    if (depth == 0)
-      tiles[x].innerHTML = "";
-    if (board[x] == 0) {
-      var newboard = board.slice();
-      newboard[x] = value;
-      var moveval = -aiturn(newboard, depth + 1, !player, false);
-      if (depth == 0)
-        tiles[x].innerHTML = moveval;
-      if (moveval > max) {
-        max = moveval;
-        index = x;
+    stateOfGame = winCheck(board);
+    if (stateOfGame) {
+      if (stateOfGame === computerVariable) {
+        score2 += 1;
+        alert("Computer wins!");
+        reset();
       }
     }
+  });
+
+  function minmaxAlg(newboard, depth, player) {
+    var stateOfGame = winCheck(newboard);
+    if (stateOfGame === false) {
+      var scores = [];
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          var boardCopy = _.cloneDeep(newboard);
+          if (boardCopy[i][j] !== " ") continue;
+          boardCopy[i][j] = player;
+          var value = minmaxAlg(boardCopy, depth + 1, ((player === playerVariable) ? computerVariable : playerVariable));
+          scores.push({
+            price: value,
+            cell: {
+              i: i,
+              j: j
+            }
+          });
+        }
+      }
+      if (player === computerVariable) {
+        var max = _.maxBy(scores, function(option) {
+          return option.price;
+        });
+        if (depth === 0) {
+          return max.cell;
+        } else {
+          return max.price;
+        }
+      } else {
+        var min = _.minBy(scores, function(option) {
+          return option.price;
+        });
+        if (depth === 0) {
+          return min.cell;
+        } else {
+          return min.price;
+        }
+      }
+    } else if (stateOfGame === null) {
+      return 0;
+    } else if (stateOfGame === playerVariable) {
+      return depth - 10;
+    } else if (stateOfGame === computerVariable) {
+      return 10 - depth;
+    }
   }
-  if (turn)
-    set(index, player)
-  return max;
-}
+
+  function aiTurn() {
+    return minmaxAlg(board, 0, computerVariable);
+  }
+
+  function winCheck(board) {
+    // Horizontal win checking
+    for (var i = 0; i < 3; i++) {
+      if (board[i][0] !== " " &&
+        board[i][0] === board[i][1] &&
+        board[i][0] === board[i][2]) {
+        return board[i][0];
+      }
+    }
+    // Vertical win checking
+    for (var j = 0; j < 3; j++) {
+      if (board[0][j] !== " " &&
+        board[0][j] === board[1][j] &&
+        board[0][j] === board[2][j]) {
+        return board[0][j];
+      }
+    }
+    // Diagonal - top left win checking
+    if (board[0][0] !== " " &&
+      board[0][0] === board[1][1] &&
+      board[0][0] === board[2][2]) {
+      return board[0][0];
+    }
+    // Diagonal - bottom left win checking
+    if (board[2][0] !== " " &&
+      board[2][0] === board[1][1] &&
+      board[2][0] === board[0][2]) {
+      return board[2][0];
+    }
+    //Verify that there are not any empty spaces, if true and no winner then the game is a tie.
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        if (board[i][j] === " ") {
+          return false;
+        }
+      }
+    }
+    return null;
+  }
+
+  function reset () {
+
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        board[i][j] = " ";
+        $('.square[data-i=' + i + '][data-j=' + j + ']').html(" ");
+      }
+    }
+    var pnScores1 = document.getElementById('PnScores1');
+    pnScores1.innerHTML = score1;
+    var pnScores2 = document.getElementById('PnScores2');
+    pnScores2.innerHTML = score2;
+  }
+
+});
