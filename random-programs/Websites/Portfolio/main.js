d865faa9cc0584bc79df75c64f815f184265b57f
Daniel Ramsayer <danielramsayer@Daniels-MacBook-Pro.local>
2017-09-06 16:11:08 -0700

    Portfolio Website stuff

---
+++ random-programs/Websites/Portfolio/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('.accordionButton').click(function() {
+		$('.accordionButton').removeClass('on');
+	 	$('.accordionContent').slideUp('normal');
+		if($(this).next().is(':hidden') == true) {
+			$(this).addClass('on');
+			$(this).next().slideDown('normal');
+		 }
+	 });
+	$('.accordionButton').mouseover(function() {
+		$(this).addClass('over');
+	}).mouseout(function() {
+		$(this).removeClass('over');
+	});
+	$('.accordionContent').hide();
+});
