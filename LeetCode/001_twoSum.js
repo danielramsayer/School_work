b975877202c0af45a9978a1aaee2d7abbfb4bbcb
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-09-28 12:37:24 -0700

    refactor to not count same indeces for both vars.

--- LeetCode/001_twoSum.js
+++ LeetCode/001_twoSum.js
@@ -10,7 +10,8 @@ return [0, 1]. */
 
 function twoSum (nums, target) {
     for (var i = 0; i < nums.length; i++) {
-        for (var j = 1; j < nums.length; j++) {
+        var checker = i+1;
+        for (var j = checker; j < nums.length; j++) {
             if(nums[i]+nums[j]==target) {
                 return [i,j];
             }
