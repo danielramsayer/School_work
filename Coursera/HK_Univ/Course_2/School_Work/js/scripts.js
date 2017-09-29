d776dd420546aa1498ae97dd564585faad72bf0d
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-09-29 15:31:50 -0700

    npm scripts part 1

---
+++ Coursera/HK_Univ/Course_2/School_Work/js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('[data-toggle="weekend"]').popover();
+  $('#mycarousel').carousel({
+    interval: 500
+  });
+  $("#carousel-button").click(function() {
+    if ($("#carousel-button").children("span").hasClass("fa-pause")) {
+      $("#mycarousel").carousel("pause");
+      $("#carousel-button").children("span").removeClass("fa-pause");
+      $("#carousel-button").children("span").addClass("fa-play");
+    } else if ($("#carousel-button").children("span").hasClass("fa-play")) {
+      $("#mycarousel").carousel("cycle");
+      $("#carousel-button").children("span").removeClass("fa-play");
+      $("#carousel-button").children("span").addClass("fa-pause");
+    }
+  });
+
+})
