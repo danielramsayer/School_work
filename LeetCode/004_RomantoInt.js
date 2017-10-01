685a4e70b233592375b395030e69dcefad34ee84
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-10-01 12:24:18 -0700

    Roman to Int javascript set up

---
+++ LeetCode/004_RomantoInt.js
@@ -0,0 +1,12 @@
+/*
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    
+};
