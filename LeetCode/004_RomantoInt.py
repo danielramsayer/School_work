3051323ca1062b4bd0c9654f4ea3f61bdecdc8e1
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-10-01 13:15:39 -0700

    Working version. Will refactor for prettiness.

--- LeetCode/004_RomantoInt.py
+++ LeetCode/004_RomantoInt.py
@@ -5,3 +5,36 @@ Input is guaranteed to be within the range from 1 to 3999.
 """
 class Solution(object):
     def romanToInt(self, s):
+        counter = 0
+        for letter in s:
+            if letter == "M":
+                counter += 1000
+            elif letter == "D":
+                counter += 500
+            elif letter == "C":
+                counter += 100
+            elif letter == "L":
+                counter += 50
+            elif letter == "X":
+                counter += 10
+            elif letter == "V":
+                counter += 5
+            elif letter == "I":
+                counter += 1
+            else:
+                return "I'm sorry, that wasn't a roman numberal"
+        if "IV" in s:
+            counter -= 2
+        if "IX" in s:
+            counter -= 2
+        if "XL" in s:
+            counter -= 20
+        if "XC" in s:
+            counter -= 20
+        if "CD" in s:
+            counter -= 200
+        if "CM" in s:
+            counter -= 200
+
+        return counter
+        
