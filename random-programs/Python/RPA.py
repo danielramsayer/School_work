10efbc0449fd7a8460912722187a4150db0d7c65
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-10-22 20:20:25 -0700

    Added notes

--- random-programs/Python/RPA.py
+++ random-programs/Python/RPA.py
@@ -3,6 +3,7 @@ def Peasants (a,b):
     x = a; y = b
     z = 0
     while x > 0:
+        #This timesing algorythm works by selecting if x is even, halving that, then doubling y. If x is odd, the value of y is added to z, y is doubled for the next iteration.
         if x % 2 = 1: z = z + y
         y = y << 1
         x = x >> 1
