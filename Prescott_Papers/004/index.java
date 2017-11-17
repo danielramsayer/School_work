655ae2f8a838487c21659f14c2b276dd0dd0278e
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-11-17 01:52:14 -0800

    minor update

---
+++ Prescott_Papers/004/index.java
@@ -0,0 +1,16 @@
+/************************
+ * Name:
+ * Program name: Index.java
+ * Date: 18 Oct 17
+ * Purpose: file reading and writing program.
+ */
+import java.io.FileReader;
+
+public class ReadnWrite {
+  public static void(String [] args) {
+
+    //List the file to be opened.
+    String fname = "TheNameOfTheFile.txt";
+
+  }
+}
