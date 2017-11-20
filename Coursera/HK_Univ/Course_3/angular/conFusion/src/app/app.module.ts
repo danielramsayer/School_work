0738523b6d51951a6edd24267aaf53c4ee6bbcd4
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-11-20 14:11:38 -0800

    update

--- Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
+++ Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 //import { FlexLayoutModule } from 'angular/flex-layout';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 
@@ -14,8 +14,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
     BrowserAnimationsModule,
     MaterialModule
     //FlexLayoutModule
