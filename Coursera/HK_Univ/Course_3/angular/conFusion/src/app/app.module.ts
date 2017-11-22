c595dd2de50ce2459ef896c512a7586a99dcc803
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-11-21 16:13:03 -0800

    src updateage.

--- Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
+++ Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from 'angular/flex-layout';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
@@ -13,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
