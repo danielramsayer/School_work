2d39dadf7f87764462922aa50028f37940515b68
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-11-22 16:00:57 -0800

    Updates - still not working correctly flex-layout

--- Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
+++ Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
