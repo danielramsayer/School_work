97625c5142a15249fd946a8af694ddf5b726296b
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-11-23 10:47:07 -0800

    Angular components update.

--- Coursera/HK_Univ/Course_3/angular/conFusion/src/app/menu/menu.component.ts
+++ Coursera/HK_Univ/Course_3/angular/conFusion/src/app/menu/menu.component.ts
@@ -2,14 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
-})
-export class MenuComponent implements OnInit {
-
-  dishes: Dish[] = [
+const DISHES: Dish[] = [
     {
       name: 'Uthappizza',
       image: '/assets/images/uthappizza.png',
@@ -43,6 +36,17 @@ export class MenuComponent implements OnInit {
       description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
     }
   ];
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  dishes = DISHES;
+
+  selectedDish: Dish = DISHES[0];
 
   constructor() { }
 
