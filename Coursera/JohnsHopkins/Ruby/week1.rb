f89e418a208b2f7d2941b8a0cb46dc16435e5e6b
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-12-18 16:44:16 -0800

    update

---
+++ Coursera/JohnsHopkins/Ruby/week1.rb
@@ -0,0 +1,19 @@
+#Lesson 1
+
+
+
+#Lesson 2
+
+a = 5
+
+if a == 4
+  puts "a is 4"
+elsif a == 5
+  puts "a is 5"
+else
+  puts "a is neither 4 nor 5"
+end
+
+times_2 = 2
+times_2 *= 2 and puts times_2 while times_2 < 100
+puts times_2
