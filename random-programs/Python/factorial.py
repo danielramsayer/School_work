5e74d55782c9f0f0ddfbf4e46bf7f3cd5f22119e
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2017-12-29 10:30:49 -0800

    coding challenge

---
+++ random-programs/Python/factorial.py
@@ -0,0 +1,11 @@
+#Some easy warm up code from CodeByte
+def FirstFactorial(num):
+    total = 1
+    while num >= 1:
+        total *= num
+        num -= 1
+    return total
+
+
+# keep this function call here
+print FirstFactorial(raw_input())
