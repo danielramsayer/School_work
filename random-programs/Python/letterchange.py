ec37edd8eb0162f527f7493c178f87b3fd59b070
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-01-02 10:54:50 -0800

    codebyte program

---
+++ random-programs/Python/letterchange.py
@@ -0,0 +1,19 @@
+def LetterChanges(str):
+    newthing = ""
+    for word in str:
+        for letter in word:
+            if letter.isalpha()==False:
+                newthing += letter
+            elif letter != 'z':
+                letter = chr(ord(letter) + 1)
+                if (letter == 'a' or letter =='e' or letter =='i' or letter =='o' or letter =='u' or letter =='y'):
+                    letter = letter.upper()
+                newthing += letter
+            else:
+                letter = 'A'
+                newthing += letter
+    # code goes here
+    return newthing
+
+# keep this function call here
+print LetterChanges(raw_input())
