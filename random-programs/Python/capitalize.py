e424604a0b3306b4c969674de665bd4289cf12f5
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-01-04 11:08:14 -0800

    comment added

--- random-programs/Python/capitalize.py
+++ random-programs/Python/capitalize.py
@@ -1,5 +1,6 @@
 #Some easy warm up code from CodeByte
 def LetterCapitalize(str):
+    #I'm sure that they wanted something else, but this works.
     return str.title()
 
 print LetterCapitalize(raw_input())
