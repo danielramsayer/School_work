60c890a0920500ae2db5e57bb41d74c6581a5e3e
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-02-03 14:56:30 -0800

    Adding second dish

--- Coursera/HK_Univ/Course_3/angular/conFusion/src/app/shared/dishes.ts
+++ Coursera/HK_Univ/Course_3/angular/conFusion/src/app/shared/dishes.ts
@@ -39,4 +39,44 @@ const DISHES: Dish[] = [
       date: "2011-12-02T17:57:28.556094Z"
     }
   ]
+  {
+    name: 'Zucchipakoda',
+    image: '/assets/images/zucchipakoda.png',
+    category: 'appetizer',
+    label: '',
+    price: '1.99',
+    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+    comments: [
+      {
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+      },
+      {
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+      }
+    ]
+  },
 ]
