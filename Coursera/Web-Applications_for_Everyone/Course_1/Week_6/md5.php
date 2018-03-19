9fdd551a3100f9bb41b1eb47e9eb0063839eb2b3
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-03-19 16:45:19 -0700

    Create!

---
+++ Coursera/Web-Applications_for_Everyone/Course_1/Week_6/md5.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Daniel Rams MD5 Cracker</title></head>
+<body>
+<h1>MD5 cracker</h1>
+<p>This application takes an MD5 hash of a four digit pin and check all 10,000 possible four digit PINs to determine the PIN.</p>
+<pre>
+Debug Output:
+<?php
+$goodtext = "Not found";
+
+?>
+</pre>
+<!-- Use the very short syntax and call htmlentities() -->
+<p>Original Text: <?= htmlentities($goodtext); ?></p>
+<form>
+<input type="text" name="md5" size="60" />
+<input type="submit" value="Crack MD5"/>
+</form>
+<ul>
+<li><a href="md5.php">Reset</a></li>
+<li><a href="index.php">Back to Cracking</a></li>
+<li><a
+href="https://github.com/csev/wa4e/tree/master/code/crack"
+target="_blank">Source code for this application</a></li>
+</ul>
+</body>
+</html>
