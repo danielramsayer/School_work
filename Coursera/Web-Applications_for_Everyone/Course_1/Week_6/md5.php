79d2d2e2e221e0c10e3ee563653c62854837d695
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-03-21 12:19:15 -0700

    MD5 updates

--- Coursera/Web-Applications_for_Everyone/Course_1/Week_6/md5.php
+++ Coursera/Web-Applications_for_Everyone/Course_1/Week_6/md5.php
@@ -4,14 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Daniel Rams MD5 Cracker</title></head>
+<title>Daniel Rams MD5 PIN Maker</title></head>
 <body>
-<h1>MD5 cracker</h1>
+<h1>MD5 PIN Maker</h1>
 <p>This application takes an MD5 hash of a four digit pin and check all 10,000 possible four digit PINs to determine the PIN.</p>
 <pre>
 Debug Output:
 <?php
-$goodtext = "Not found";
+  $goodtext = "Not found";
 
 ?>
 </pre>
