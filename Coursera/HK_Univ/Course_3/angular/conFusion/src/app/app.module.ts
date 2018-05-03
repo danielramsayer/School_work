cfc46987bd7087e7a651bda6af4007883d31a07b
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-05-03 15:58:33 -0700

    Update

--- Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
+++ Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +28,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [ DishService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
