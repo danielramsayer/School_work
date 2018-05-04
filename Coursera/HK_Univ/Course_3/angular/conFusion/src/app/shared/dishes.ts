2b1168e171b4ef07ce8de02d09cc32ba6fa0b624
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-05-04 10:47:06 -0700

    Testing for bug

--- Coursera/HK_Univ/Course_3/angular/conFusion/src/app/shared/dishes.ts
+++ Coursera/HK_Univ/Course_3/angular/conFusion/src/app/shared/dishes.ts
@@ -10,31 +10,31 @@ export const DISHES: Dish[] = [
     description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
     comments: [
       {
-        rating: 5,
+        rating: 1,
         comment: "Imagine all the eatables, living in conFusion!",
         author: "John Lemon",
         date: "2012-10-16T17:57:28.556094Z"
       },
       {
-        rating: 4,
+        rating: 1,
         comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
         author: "Paul McVites",
         date: "2014-09-05T17:57:28.556094Z"
       },
       {
-        rating: 3,
+        rating: 1,
         comment: "Eat it, just eat it!",
         author: "Michael Jaikishan",
         date: "2015-02-13T17:57:28.556094Z"
       },
       {
-        rating: 4,
+        rating: 1,
         comment: "Ultimate, Reaching for the stars!",
         author: "Ringo Starry",
         date: "2013-12-02T17:57:28.556094Z"
       },
       {
-        rating: 2,
+        rating: 1,
         comment: "It's your birthday, we're gonna party!",
         author: "25 Cent",
         date: "2011-12-02T17:57:28.556094Z"
@@ -50,25 +50,25 @@ export const DISHES: Dish[] = [
     description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
     comments: [
       {
-        rating: 5,
+        rating: 2,
         comment: "Imagine all the eatables, living in conFusion!",
         author: "John Lemon",
         date: "2012-10-16T17:57:28.556094Z"
       },
       {
-        rating: 4,
+        rating: 2,
         comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
         author: "Paul McVites",
         date: "2014-09-05T17:57:28.556094Z"
       },
       {
-        rating: 3,
+        rating: 2,
         comment: "Eat it, just eat it!",
         author: "Michael Jaikishan",
         date: "2015-02-13T17:57:28.556094Z"
       },
       {
-        rating: 4,
+        rating: 2,
         comment: "Ultimate, Reaching for the stars!",
         author: "Ringo Starry",
         date: "2013-12-02T17:57:28.556094Z"
@@ -90,13 +90,13 @@ export const DISHES: Dish[] = [
     description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
     comments: [
       {
-        rating: 5,
+        rating: 3,
         comment: "Imagine all the eatables, living in conFusion!",
         author: "John Lemon",
         date: "2012-10-16T17:57:28.556094Z"
       },
       {
-        rating: 4,
+        rating: 3,
         comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
         author: "Paul McVites",
         date: "2014-09-05T17:57:28.556094Z"
@@ -108,13 +108,13 @@ export const DISHES: Dish[] = [
         date: "2015-02-13T17:57:28.556094Z"
       },
       {
-        rating: 4,
+        rating: 3,
         comment: "Ultimate, Reaching for the stars!",
         author: "Ringo Starry",
         date: "2013-12-02T17:57:28.556094Z"
       },
       {
-        rating: 2,
+        rating: 3,
         comment: "It's your birthday, we're gonna party!",
         author: "25 Cent",
         date: "2011-12-02T17:57:28.556094Z"
@@ -130,7 +130,7 @@ export const DISHES: Dish[] = [
     description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
     comments: [
       {
-        rating: 5,
+        rating: 4,
         comment: "Imagine all the eatables, living in conFusion!",
         author: "John Lemon",
         date: "2012-10-16T17:57:28.556094Z"
@@ -142,7 +142,7 @@ export const DISHES: Dish[] = [
         date: "2014-09-05T17:57:28.556094Z"
       },
       {
-        rating: 3,
+        rating: 4,
         comment: "Eat it, just eat it!",
         author: "Michael Jaikishan",
         date: "2015-02-13T17:57:28.556094Z"
@@ -154,7 +154,7 @@ export const DISHES: Dish[] = [
         date: "2013-12-02T17:57:28.556094Z"
       },
       {
-        rating: 2,
+        rating: 4,
         comment: "It's your birthday, we're gonna party!",
         author: "25 Cent",
         date: "2011-12-02T17:57:28.556094Z"
