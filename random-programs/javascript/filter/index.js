e27bf3d5921ca1cee4edebf0acd408657cb941ea
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-05-17 16:50:43 -0700

    updating the index file

--- random-programs/javascript/filter/index.js
+++ random-programs/javascript/filter/index.js
@@ -14,6 +14,16 @@ function applyFilter() {
 	  });
 }
 
+function applySecondFilter() {
+	Caman('#my-image', function () {
+	    this.brightness(0);
+	    this.contrast(100);
+	    this.sepia(30);
+	    this.saturation(30);
+	    this.render();
+	  });
+}
+
 // function applyFilter() {
 // 	Caman('#my-image', function () {
 // 	    this['vintage']();
