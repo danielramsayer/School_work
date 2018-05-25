1aceacd100bd3b9e1f5141ed8c95d3e7c0b7cf7f
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-05-24 18:14:06 -0700

    Routing information

--- Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
+++ Coursera/HK_Univ/Course_3/angular/conFusion/src/app/app.module.ts
@@ -11,13 +11,15 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,8 @@ import { ContactComponent } from './contact/contact.component';
     FormsModule,
     HttpModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [ DishService ],
   bootstrap: [AppComponent]
