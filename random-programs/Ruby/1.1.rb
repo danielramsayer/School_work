0c51e100b2000d3f8b1b1ba8cc0745ed4db68463
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-06-07 10:44:44 -0700

    Things!

--- random-programs/Ruby/1.1.rb
+++ random-programs/Ruby/1.1.rb
@@ -1,21 +1,25 @@
 #Is a year a leap year? Because the actual length of a year is 365.25635 and not a round number, every 4 years instead of 365 days we have 366. This program is designed to determine if an entry year is a lead year or not.
-puts "Which year am I checking for leap year status? "
-year_to_check = gets.to_i
-def leapyear_checker(checking)
+
+def leapyear_checker()
+  puts "Which year am I checking for leap year status? "
+  checking = gets.to_i
+  if checking < 0
+    puts "Ah! Checking into the BC era are we? Let me see..."
+  end
   if (checking % 4 == 0) && (checking % 100 != 0) || (checking % 400 == 0)
-    puts checking.to_s + " is a leap year. "
-    puts (checking + 4).to_s + " is the next leap year."
+    puts checking.to_s.gsub(/-/, "BC ") + " is a leap year. "
+    puts (checking + 4).to_s.gsub(/-/, "BC ") + " is the next leap year."
   else
-    puts checking.to_s + " is not a leapyear. "
+    puts checking.to_s.gsub(/-/, "BC ") + " is not a leapyear. "
     checking2 = checking
     cycle = checking + 3
     until checking2 > cycle
       checking2 += 1
       if (checking2 % 4 == 0) && (checking2 % 100 != 0) || (checking2 % 400 == 0)
-        puts "However, " + checking2.to_s + " is a leap year. "
+        puts "However, " + checking2.to_s.gsub(/-/, "BC ") + " is a leap year. "
       end
     end
   end
 end
 
-leapyear_checker(year_to_check)
+leapyear_checker()
---
+++ random-programs/Ruby/11readme.md
@@ -0,0 +1,18 @@
+### Ruby - Leap Year Checker
+***
+
+#### Objective
+Create a checker that verifies if an input year is a leap year, and if not gives you the next leap year. This was also intended to work from Time Immemorial, so BC works as well. It even lets you know if you've gone back in time.
+
+#### Steps Taken
+First I created the logic for what /is/ a leap year. Afterwards I was created conditional formatting to capture if the number met that logic or didn't. From there I incremented the year until the next year that met the leap year requirements.
+
+Here is the code for [Leap Year Checker](https://github.com/danielramsayer/School_work/blob/master/random-programs/Ruby/1.1.rb).
+
+
+#### The Result
+
+The leap year checker works well and was a fun project to do. Perhaps a birthday clock is in the future to determine peoples zodiacs?
+
+
+[Return to portfolio](https://github.com/danielramsayer/Portfolio)
