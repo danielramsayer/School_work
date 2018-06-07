a12d8aef3209e574740afd6001db0985f69ffecb
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-06-07 10:15:55 -0700

    Leap year checker complete.

--- random-programs/Ruby/1.1.rb
+++ random-programs/Ruby/1.1.rb
@@ -1,17 +1,21 @@
 #Is a year a leap year? Because the actual length of a year is 365.25635 and not a round number, every 4 years instead of 365 days we have 366. This program is designed to determine if an entry year is a lead year or not.
-puts "Please enter a year that you are looking to check if "
+puts "Which year am I checking for leap year status? "
 year_to_check = gets.to_i
-
-if (year_to_check % 4 == 0) && (year_to_check % 100 == 0) && (year_to_check % 400 == 0)
-  print year_to_check.to_s + " is infact a leap year. "
-  puts (year_to_check + 4).to_s + " is the next leap year."
-else
-  print year_to_check.to_s + " is not a leapyear. "
-  year_to_check2 = year_to_check
-  loop do
-    year_to_check2 += 1
-    next unless ((year_to_check2 % 4 == 0) && (year_to_check2 % 100 == 0) && (year_to_check2 % 400 == 0)) == 0
-    puts year_to_check2.to_s + " is the next leap year."
-    break if year_to_check2 >= year_to_check+4
+def leapyear_checker(checking)
+  if (checking % 4 == 0) && (checking % 100 != 0) || (checking % 400 == 0)
+    puts checking.to_s + " is a leap year. "
+    puts (checking + 4).to_s + " is the next leap year."
+  else
+    puts checking.to_s + " is not a leapyear. "
+    checking2 = checking
+    cycle = checking + 3
+    until checking2 > cycle
+      checking2 += 1
+      if (checking2 % 4 == 0) && (checking2 % 100 != 0) || (checking2 % 400 == 0)
+        puts "However, " + checking2.to_s + " is a leap year. "
+      end
+    end
   end
 end
+
+leapyear_checker(year_to_check)
