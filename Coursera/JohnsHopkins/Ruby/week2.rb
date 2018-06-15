3a78a48e8137abf2a1c86c780c8ee7a089d09c4f
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-06-15 13:15:00 -0700

    Adding second example

--- Coursera/JohnsHopkins/Ruby/week2.rb
+++ Coursera/JohnsHopkins/Ruby/week2.rb
@@ -15,3 +15,26 @@ person1 = Person.new("Jacob", 12, "cats")
 p person1.instance_variables
 puts person1.get_info
 p person1.instance_variables
+
+
+#Getter and Setter Access
+
+class Person
+  def initialize (name, age, pet)
+    @name = name
+    @age = age
+    @pet = pet
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+end
+
+person1 = Person.new("Jacob", 12, "cats")
+puts person1.name
+person1.name = "Mike"
+puts person1.name
+p person1.instance_variables
