802487bfd57c40d3fa5c09500f1156f9e7b6df94
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-06-15 13:15:42 -0700

    Committing!

--- Coursera/JohnsHopkins/Ruby/week2.rb
+++ Coursera/JohnsHopkins/Ruby/week2.rb
@@ -38,3 +38,19 @@ puts person1.name
 person1.name = "Mike"
 puts person1.name
 p person1.instance_variables
+
+#Attribute getter and setter methods
+class Person2
+  attr_accessor :name, :age, :pet
+end
+
+person1 = Person2.new
+p person1.name
+person1.name = "Darius"
+person1.age = "18"
+person1.pet = "Guinnea Pig"
+puts person1.name
+puts person1.age
+puts person1.pet
+person1.pet = "dog"
+puts person1.pet
