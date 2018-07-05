e2c7fe926b701bbaf449302d80208ceaa9df3bde
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-07-05 12:07:26 -0700

    Script for street images is working.

--- Udacity/Ajax/js/script.js
+++ Udacity/Ajax/js/script.js
@@ -1,18 +1,26 @@
 
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+    const $body = $('body');
+    const $wikiElem = $('#wikipedia-links');
+    const $nytHeaderElem = $('#nytimes-header');
+    const $nytElem = $('#nytimes-articles');
+    const $greeting = $('#greeting');
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
+    let streetvar = $('#street').val();
+    console.log(streetvar)
+    let cityvar = $('#city').val();
+    let address = streetvar + ", " + cityvar;
 
+    $greeting.text("Is this where you want to live? " + address);
+
+    let streetImgUrl = "https://maps.googleapis.com/maps/api/streetview?size=650x450&location=" + address;
+    $body.append('<img class="bgimg" src="' + streetImgUrl + '">')
     // YOUR CODE GOES HERE!
 
     return false;
