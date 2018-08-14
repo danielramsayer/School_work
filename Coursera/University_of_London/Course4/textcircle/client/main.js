ee8518a78ceda1e60a17af5f8887ae1b7a97ab78
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-08-14 15:18:16 -0700

    Working on making work.

--- Coursera/University_of_London/Course4/textcircle/client/main.js
+++ Coursera/University_of_London/Course4/textcircle/client/main.js
@@ -3,20 +3,31 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+/*Template.greeting.onCreated(function greetingOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.greeting.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.greeting.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+*/
+this.Documents = new Mongo.Collection("documents");
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    //This code is run by the server on startup.
+    if (!Documents.findOne()){ //if there isn't a document yet
+      Documents.insert({title:"My new document."});
+    }
+  });
+}
