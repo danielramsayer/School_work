62428690451e563999b77636a4a7bee1e08e76e6
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-08-17 17:02:55 -0700

    working on bug

--- Coursera/University_of_London/Course4/textcircle/client/main.js
+++ Coursera/University_of_London/Course4/textcircle/client/main.js
@@ -1,33 +1,40 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import { Mongo } from 'meteor/mongo';
 import './main.html';
-/*
-Template.greeting.onCreated(function greetingOnCreated() {
+
+const Documents = new Mongo.Collection("documents");
+console.log(Documents);
+
+if (!Meteor.isServer) {
+  console.log("Meteor is not the server");
+}
+
+if (Meteor.isServer) {
+  console.log("Server is running");
+  Meteor.startup(function () {
+    console.log("startup is running");
+    if (!Documents.findOne()){
+      console.log("findone exists");
+      Documents.insert({title:"My new document"});
+    }
+  });
+}
+
+Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.greeting.helpers({
+Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.greeting.events({
+Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
 });
-*/
-this.Documents = new Mongo.Collection("documents");
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    //This code is run by the server on startup.
-    if (!Documents.findOne()){ //if there isn't a document yet
-      Documents.insert({title:"My new document."});
-    }
-  });
-}
