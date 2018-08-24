b53e335bd4fd78f06f9859f4ada5241d4322999a
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-08-23 18:55:44 -0700

    commit

--- Coursera/University_of_London/Course4/textcircle/client/main.js
+++ Coursera/University_of_London/Course4/textcircle/client/main.js
@@ -6,12 +6,21 @@ import './main.html';
 this.Documents = new Mongo.Collection("documents");
 console.log(Documents);
 
+if (Meteor.isClient) {
+  console.log("1");
+  Template.editor.helpers({
+    console.log("2");
+    docid:function(){
+      return Documents._collection.findOne()._id;
+    }
+  });
+}
+
 Meteor.startup(function() {
   if (!Documents.findOne()){
     Documents._collection.insert({title:"My new document!"});
   }
 });
-
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
