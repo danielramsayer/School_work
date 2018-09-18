c4fb5ee22cf281bc1998455ec17552169dc9a27c
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-09-18 14:50:47 -0700

    fiddling.

--- Coursera/HK_Univ/Course_4/git-test/src/App.js
+++ Coursera/HK_Univ/Course_4/git-test/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. This can be tricky! Does this work? That is a question.Whether tis nobl'r to suffer slings and the arrows.
+          To get started, edit <code>src/App.js</code> and save to reload. 
         </p>
       </div>
     );
