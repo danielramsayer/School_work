48b9c032da4f6756f726432f89aa1f696564396e
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-09-21 15:22:00 -0700

    Getting the menu working.

--- Coursera/HK_Univ/Course_4/git-test/src/components/MenuComponent.js
+++ Coursera/HK_Univ/Course_4/git-test/src/components/MenuComponent.js
@@ -44,9 +44,17 @@ class Menu extends Component {
   }
   render() {
     const menu = this.state.dishes.map((dish) => {
-      return(
+      return (
         <div key={dish.id} className="col-12 mt-5">
-          
+          <Media tag="li">
+            <Media left middle>
+              <Media object src={dish.image} alt={dish.name} />
+            </Media>
+            <Media body className="ml-5">
+              <Media heading>{dish.name}</Media>
+              <p>{dish.description}</p>
+            </Media>
+          </Media>
         </div>
       );
     });
