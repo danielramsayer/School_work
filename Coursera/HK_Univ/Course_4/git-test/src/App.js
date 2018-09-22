77377cf95f49612556c4e0db9709b5d94d6200ae
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-09-22 14:39:59 -0700

    Correcting

--- Coursera/HK_Univ/Course_4/git-test/src/App.js
+++ Coursera/HK_Univ/Course_4/git-test/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand href="/">Ristorante Con Fusion </NavbarBrand>
           </div>
         </Navbar>
         <Menu />
