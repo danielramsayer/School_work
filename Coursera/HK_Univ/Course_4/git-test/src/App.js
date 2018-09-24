b1f2364ff34e201152944f88430cd4c2087cd69f
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-09-24 13:12:57 -0700

    Making this work with dishes in a seporate file.

--- Coursera/HK_Univ/Course_4/git-test/src/App.js
+++ Coursera/HK_Univ/Course_4/git-test/src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +20,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion </NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
