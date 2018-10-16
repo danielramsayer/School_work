64594f9dd775d8ac2610cef383c02e7864612569
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-10-16 04:21:15 -0700

    Distinguishing similiar comments.

--- Coursera/HK_Univ/Course_4/git-test/src/shared/dishes.js
+++ Coursera/HK_Univ/Course_4/git-test/src/shared/dishes.js
@@ -12,7 +12,7 @@ export const DISHES =
       {
       id: 0,
       rating: 5,
-      comment: "Imagine all the eatables, living in conFusion!",
+      comment: "0Imagine all the eatables, living in conFusion!",
       author: "John Lemon",
       date: "2012-10-16T17:57:28.556094Z"
       },
@@ -57,7 +57,7 @@ export const DISHES =
       {
       id: 0,
       rating: 5,
-      comment: "Imagine all the eatables, living in conFusion!",
+      comment: "1Imagine all the eatables, living in conFusion!",
       author: "John Lemon",
       date: "2012-10-16T17:57:28.556094Z"
       },
@@ -103,7 +103,7 @@ export const DISHES =
       {
       id: 0,
       rating: 5,
-      comment: "Imagine all the eatables, living in conFusion!",
+      comment: "2Imagine all the eatables, living in conFusion!",
       author: "John Lemon",
       date: "2012-10-16T17:57:28.556094Z"
       },
@@ -149,7 +149,7 @@ export const DISHES =
       {
       id: 0,
       rating: 5,
-      comment: "Imagine all the eatables, living in conFusion!",
+      comment: "3Imagine all the eatables, living in conFusion!",
       author: "John Lemon",
       date: "2012-10-16T17:57:28.556094Z"
       },
