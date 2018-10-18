b58b0e64f6d03820e56e37a5f8fdf8aaeb107460
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-10-17 19:53:08 -0700

    Update for new Main Component

--- random-programs/Random_React/random_react/src/App.js
+++ random-programs/Random_React/random_react/src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Age from './components/age';
+import {Header} from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <img src={logo} className="App-logo" alt="logo" align="left"/>
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <body>
+          <div>
+            <p>Their age is: <Age age={7}/></p>
+          </div>
+        </body>
       </div>
     );
   }
