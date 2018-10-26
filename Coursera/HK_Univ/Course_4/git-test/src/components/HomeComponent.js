88c438966e5404f3ee237167cf9b95e02f2d2881
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-10-25 19:16:01 -0700

    Update component for HW 2

--- Coursera/HK_Univ/Course_4/git-test/src/components/HomeComponent.js
+++ Coursera/HK_Univ/Course_4/git-test/src/components/HomeComponent.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
 function RenderCard({item}) {
-  //console.table(item);
   return (
     <Card>
       <CardImg src={item.image} alt={item.name} />
