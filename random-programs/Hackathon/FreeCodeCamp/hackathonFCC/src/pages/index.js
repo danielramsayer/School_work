d241fba2257c77077293197bf393e445b32d305b
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-11-01 20:44:50 -0700

    Pages

---
+++ random-programs/Hackathon/FreeCodeCamp/hackathonFCC/src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello There Darlings!</h1>
+    <p>Welcome to our new Gatsby site. Please, look around! Explore, and enjoy! </p>
+    <p>We're here to build something great.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
