c6a1f85893f08d3284321eec9c1ab6598845bc8a
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-11-12 16:56:30 -0800

    Making it into a function

--- random-programs/Python/fizzbuzz.py
+++ random-programs/Python/fizzbuzz.py
@@ -1,7 +1,12 @@
-for number in range(100):
-    if number % 3 == 0 and number % 5 == 0:
-        print number, "fizzbuzz"
-    elif number % 5 == 0:
-        print number, "buzz"
-    elif number % 3 == 0:
-        print number, "fizz"
+def fizzbuzz():
+    for number in range(100):
+        if number % 3 == 0 and number % 5 == 0:
+            print number, "fizzbuzz"
+        elif number % 5 == 0:
+            print number, "buzz"
+        elif number % 3 == 0:
+            print number, "fizz"
+        else:
+            print number
+
+print fizzbuzz()
