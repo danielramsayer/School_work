e392feea9f5c7cf14d30cfb02f0907a419ef4c5d
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-11-13 16:55:07 -0800

    Created Test data

---
+++ random-programs/Random_React/my-new-app/src/test_stuff/test_data.js
@@ -0,0 +1,193 @@
+export const CLIENTS = [{
+    "id": 1,
+    "first_name": "Brook",
+    "last_name": "Stockford",
+    "email": "bstockford0@wikipedia.org",
+    "fursona": "Pseudocheirus peregrinus",
+    "card_number": "374288983726448"
+  },
+  {
+    "id": 2,
+    "first_name": "Gregoor",
+    "last_name": "Harrema",
+    "email": "gharrema1@goo.gl",
+    "fursona": "Phoca vitulina",
+    "card_number": "5641824606276880691"
+  },
+  {
+    "id": 3,
+    "first_name": "Gordy",
+    "last_name": "Leaney",
+    "email": "gleaney2@shop-pro.jp",
+    "fursona": "Uraeginthus bengalus",
+    "card_number": "3557166738474113"
+  },
+  {
+    "id": 4,
+    "first_name": "Melody",
+    "last_name": "Worman",
+    "email": "mworman3@odnoklassniki.ru",
+    "fursona": "Aegypius occipitalis",
+    "card_number": "5602240650448395"
+  },
+  {
+    "id": 5,
+    "first_name": "Mindy",
+    "last_name": "Milham",
+    "email": "mmilham4@about.com",
+    "fursona": "Delphinus delphis",
+    "card_number": "5048377342255812"
+  },
+  {
+    "id": 6,
+    "first_name": "Darbee",
+    "last_name": "Pedrielli",
+    "email": "dpedrielli5@simplemachines.org",
+    "fursona": "Oryx gazella",
+    "card_number": "4844094151164402"
+  },
+  {
+    "id": 7,
+    "first_name": "Bee",
+    "last_name": "Fludgate",
+    "email": "bfludgate6@simplemachines.org",
+    "fursona": "Merops bullockoides",
+    "card_number": "5020982566022902378"
+  },
+  {
+    "id": 8,
+    "first_name": "Artair",
+    "last_name": "McDuffie",
+    "email": "amcduffie7@canalblog.com",
+    "fursona": "Macropus rufogriseus",
+    "card_number": "5120830884607270"
+  },
+  {
+    "id": 9,
+    "first_name": "Danika",
+    "last_name": "Reaper",
+    "email": "dreaper8@washington.edu",
+    "fursona": "Aegypius occipitalis",
+    "card_number": "6384344185386237"
+  },
+  {
+    "id": 10,
+    "first_name": "Barrie",
+    "last_name": "Corssen",
+    "email": "bcorssen9@ocn.ne.jp",
+    "fursona": "Meles meles",
+    "card_number": "3534558100884945"
+  },
+  {
+    "id": 11,
+    "first_name": "Abbot",
+    "last_name": "Giabuzzi",
+    "email": "agiabuzzia@wisc.edu",
+    "fursona": "Crotalus cerastes",
+    "card_number": "4405090928835390"
+  },
+  {
+    "id": 12,
+    "first_name": "Carey",
+    "last_name": "Balaizot",
+    "email": "cbalaizotb@ask.com",
+    "fursona": "Corvus albus",
+    "card_number": "6304908397903653"
+  },
+  {
+    "id": 13,
+    "first_name": "Genovera",
+    "last_name": "Chaise",
+    "email": "gchaisec@trellian.com",
+    "fursona": "Mazama gouazoubira",
+    "card_number": "502067936583032511"
+  },
+  {
+    "id": 14,
+    "first_name": "Bathsheba",
+    "last_name": "Sarll",
+    "email": "bsarlld@desdev.cn",
+    "fursona": "Sarcophilus harrisii",
+    "card_number": "5002358703208156"
+  },
+  {
+    "id": 15,
+    "first_name": "Rochette",
+    "last_name": "Aldcorne",
+    "email": "raldcornee@aol.com",
+    "fursona": "Macropus giganteus",
+    "card_number": "6763616884045628864"
+  },
+  {
+    "id": 16,
+    "first_name": "Alvin",
+    "last_name": "O'Cullinane",
+    "email": "aocullinanef@bloomberg.com",
+    "fursona": "Phalacrocorax varius",
+    "card_number": "5602213765704139947"
+  },
+  {
+    "id": 17,
+    "first_name": "Rabi",
+    "last_name": "Illston",
+    "email": "rillstong@merriam-webster.com",
+    "fursona": "Psophia viridis",
+    "card_number": "491186723619877578"
+  },
+  {
+    "id": 18,
+    "first_name": "Maurice",
+    "last_name": "Di Gregorio",
+    "email": "mdigregorioh@constantcontact.com",
+    "fursona": "Pseudocheirus peregrinus",
+    "card_number": "6378751074302067"
+  },
+  {
+    "id": 19,
+    "first_name": "Darryl",
+    "last_name": "Mignot",
+    "email": "dmignoti@springer.com",
+    "fursona": "Dendrocygna viduata",
+    "card_number": "3574389630075353"
+  },
+  {
+    "id": 20,
+    "first_name": "Gardie",
+    "last_name": "Polini",
+    "email": "gpolinij@purevolume.com",
+    "fursona": "Alligator mississippiensis",
+    "card_number": "5610306910664752138"
+  },
+  {
+    "id": 21,
+    "first_name": "Candice",
+    "last_name": "Grouen",
+    "email": "cgrouenk@latimes.com",
+    "fursona": "Phascogale calura",
+    "card_number": "3535087693579208"
+  },
+  {
+    "id": 22,
+    "first_name": "Kelsi",
+    "last_name": "Dorken",
+    "email": "kdorkenl@msu.edu",
+    "fursona": "Ara ararauna",
+    "card_number": "56100218395127425"
+  },
+  {
+    "id": 23,
+    "first_name": "Anabella",
+    "last_name": "Pomphrey",
+    "email": "apomphreym@jalbum.net",
+    "fursona": "Phalacrocorax carbo",
+    "card_number": "3587797548702691"
+  },
+  {
+    "id": 24,
+    "first_name": "Olly",
+    "last_name": "Done",
+    "email": "odonen@bbc.co.uk",
+    "fursona": "Gekko gecko",
+    "card_number": "4405808814549720"
+  },
+]
