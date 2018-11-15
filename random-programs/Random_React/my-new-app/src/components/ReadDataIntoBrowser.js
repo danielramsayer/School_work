f9e0b681ce9474fa915befa9508e70a7f4be4a29
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-11-14 18:45:15 -0800

    Fixed

--- random-programs/Random_React/my-new-app/src/components/ReadDataIntoBrowser.js
+++ random-programs/Random_React/my-new-app/src/components/ReadDataIntoBrowser.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function ReadDataIntoBrowser({data}) {
-  let data = {data}
-  const peopleListing = data.map((people) => {
+  let data1 = data;
+  const peopleListing = data1.map((people) => {
     return (
       <div key={people.id}>
         <br />{people.last_name}, {people.first_name}
