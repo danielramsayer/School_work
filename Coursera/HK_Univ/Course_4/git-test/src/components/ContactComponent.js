829069da7b5fb41072bb0296d30c26ae8e7a07b5
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-11-17 17:47:44 -0800

    Uncontrolled form stuff.

--- Coursera/HK_Univ/Course_4/git-test/src/components/ContactComponent.js
+++ Coursera/HK_Univ/Course_4/git-test/src/components/ContactComponent.js
@@ -83,7 +83,7 @@ class Contact extends Component {
     const reg = /^\d+$/;
     if (this.state.touched.telnum && !reg.test(telnum))
       errors.telnum = 'Tel. Number should contain only numbers';
-
+      
     if (this.state.touched.email && email.split('').filter(x => x === '@').length !== 1)
       errors.email = 'Email should contain a @';
 
