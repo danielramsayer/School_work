6c40e15296455e27d0c6d54d3ecb1bca7dd8798c
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-11-29 18:22:07 -0800

    Update for redux inclusion

--- random-programs/Random_React/my-new-app/src/index.js
+++ random-programs/Random_React/my-new-app/src/index.js
@@ -5,11 +5,27 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 
-function reducer() {
+function reducer(state, actions) {
+  if (action.type === 'changeState') {
+    return action.payload.newState;
+  }
   return 'State';
 }
 
 const store = createStore(reducer);
+
+console.log(store.getState())
+
+const actions = {
+  type: 'changeState',
+  payload: {
+    newState: 'New state'
+  }
+};
+
+store.dispatch(actions);
+
+console.log(store.getstate());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
