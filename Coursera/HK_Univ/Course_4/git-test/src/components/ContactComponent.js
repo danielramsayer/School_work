5cde7c02467cdefd1ed0c2595ba514d903f87905
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-12-05 15:53:30 -0800

    Updating the handlesubmit logic

--- Coursera/HK_Univ/Course_4/git-test/src/components/ContactComponent.js
+++ Coursera/HK_Univ/Course_4/git-test/src/components/ContactComponent.js
@@ -16,10 +16,9 @@ class Contact extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event) {
-    console.log('Current State is: ' + JSON.stringify(this.state));
-    alert('Current State is: ' + JSON.stringify(this.state));
-    event.preventDefault();
+  handleSubmit(values) {
+    console.log('Current State is: ' + JSON.stringify(values));
+    alert('Current State is: ' + JSON.stringify(values));
   }
 
   render() {
