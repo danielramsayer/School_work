254685dde3b3b6e1a2661076fe8db2113f8c65f2
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-12-06 16:03:24 -0800

    Adding comment component

--- Coursera/HK_Univ/Course_4/git-test/src/components/DishdetailComponent.js
+++ Coursera/HK_Univ/Course_4/git-test/src/components/DishdetailComponent.js
@@ -1,83 +1,81 @@
 import React from 'react';
-import { Card, CardImg, CardTitle, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom'
+import CommentModal from './CommentModal';
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardBody,
+  Breadcrumb,
+  BreadcrumbItem
+} from 'reactstrap';
+import {Link} from 'react-router-dom'
 
-  function RenderDish({dish}) {
-    if (dish!=null) {
-      return (
-        <Card>
-          <CardImg width="50%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-            <CardText align="right">${dish.price}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return (
-        <div></div>
-      )
-    }
+function RenderDish({dish}) {
+  if (dish != null) {
+    return (<Card>
+      <CardImg width="50%" src={dish.image} alt={dish.name}/>
+      <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
+        <CardText>{dish.description}</CardText>
+        <CardText align="right">${dish.price}</CardText>
+      </CardBody>
+    </Card>);
+  } else {
+    return (<div></div>)
   }
+}
 
-  function RenderComments({comments}) {
-    const commentListing = comments.map((eachComment) => {
-      return (
-        <div key={eachComment.id}>
-          <div>{eachComment.comment}</div>
-          <div><p><strong>--{eachComment.author}</strong>, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(eachComment.date)))}</p></div>
-        </div>
-      )
-    });
-    return commentListing;
-  }
-
-  function commentModal() {
-    console.log("triggered");
-    return (
-      <div className="modal" id="commentModal">
-        <div className="modal-dialog">
-          <div className="modal-content"></div>
-        </div>
+function RenderComments({comments}) {
+  const commentListing = comments.map((eachComment) => {
+    return (<div key={eachComment.id}>
+      <div>{eachComment.comment}</div>
+      <div>
+        <p>
+          <strong>--{eachComment.author}</strong>, {
+            new Intl.DateTimeFormat('en-US', {
+              year: 'numeric',
+              month: 'short',
+              day: '2-digit'
+            }).format(new Date(Date.parse(eachComment.date)))
+          }</p>
       </div>
-    )
-  }
+    </div>)
+  });
+  return commentListing;
+}
 
-  const DishDetail = (props) => {
-    if(props.dish != null){
-      return (
-        <div className="container">
-          <div className="row">
-            <Breadcrumb>
-              <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-            </Breadcrumb>
-          </div>
-          <div className="col-12">
-            <h3>{props.dish.name}</h3>
-            <hr />
-          </div>
-        <div className="row">
-          <div className="col-12 col-lg-5 m-1">
-            <RenderDish dish={props.dish} />
-          </div>
-          <div>
-            <div className="col-12 col-lg-7 m-1">
-              <h2>Comments</h2>
-              <RenderComments comments={props.comments} />
-            </div>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#commentModal" onclick="commentModal()">
-
-              Submit Comment
-            </button>
+const DishDetail = (props) => {
+  if (props.dish != null) {
+    return (<div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to='/menu'>Menu</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active="active">{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
+      <div className="col-12">
+        <h3>{props.dish.name}</h3>
+        <hr/>
+      </div>
+      <div className="row">
+        <div className="col-12 col-lg-5 m-1">
+          <RenderDish dish={props.dish}/>
+        </div>
+        <div>
+          <div className="col-12 col-lg-7 m-1">
+            <h2>Comments</h2>
+            <RenderComments comments={props.comments}/>
+            <CommentModal />
           </div>
         </div>
       </div>
-      );
-    } else {
-      return (<div></div>);
-    }
+    </div>);
+  } else {
+    return (<div></div>);
   }
+}
 
 export default DishDetail;
