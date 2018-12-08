e365731a5646033b409d93353bda1c833f9d4e07
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-12-07 19:31:45 -0800

    Minor update

--- Coursera/HK_Univ/Course_4/git-test/src/components/HeaderComponent.js
+++ Coursera/HK_Univ/Course_4/git-test/src/components/HeaderComponent.js
@@ -28,6 +28,7 @@ class Header extends Component {
     this.toggleNav = this.toggleNav.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
+    console.table("Header", props);
   }
 
   toggleNav() {
