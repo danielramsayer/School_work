b36debb94f6153a0e57e089e9de34c4517ec3b98
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2018-12-21 18:19:51 -0800

    Updating for reusability

--- random-programs/Random_React/my-new-app/src/index.js
+++ random-programs/Random_React/my-new-app/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { combineReducers, createStore } from 'redux';
-
+/*
 function productsReducer(state =[], action) {
   return state;
 }
@@ -21,7 +21,7 @@ const allReducers = combineReducers({
 const store = createStore(allReducers);
 
 console.log(store.getState())
-
+*/
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
