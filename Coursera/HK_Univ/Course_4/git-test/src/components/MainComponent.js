ae361f25c01a0623982dc0cd1d2e19c269baa37b
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2019-01-02 15:08:52 -0800

    Updating for new Redux items.

--- Coursera/HK_Univ/Course_4/git-test/src/components/MainComponent.js
+++ Coursera/HK_Univ/Course_4/git-test/src/components/MainComponent.js
@@ -38,7 +38,6 @@ class Main extends Component {
     }
 
     const DishWithId = ({match}) => {
-      console.log({match});
       return (
         <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
           comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
