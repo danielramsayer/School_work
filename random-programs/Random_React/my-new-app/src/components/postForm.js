bffa4123ac409c9dcc2a50bb0b736fccd342e32c
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2019-01-11 10:45:13 -0800

    Fiddling with the state.

--- random-programs/Random_React/my-new-app/src/components/postForm.js
+++ random-programs/Random_React/my-new-app/src/components/postForm.js
@@ -4,27 +4,29 @@ class PostForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "",
-      body: ""
+      title: '',
+      body: ''
     };
+    this.onChange = this.onChange.bind(this);
   }
 
-
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
   render() {
-    console.log("This registers");
     return (
       <div>
         <h2>Add Post</h2>
         <form>
           <div>
             <label>Title: </label><br />
-            <input type="text" name="title" value={this.state.title}/>
+            <input type="text" name="title" onChange={this.onChange} value={this.state.title}/>
           </div>
           <br />
           <div>
             <label>Body: </label><br />
-            <textarea name="body" value={this.state.body}/>
+            <textarea name="body" onChange={this.onChange} value={this.state.body}/>
           </div>
           <br />
           <button type="submit">Submit</button>
