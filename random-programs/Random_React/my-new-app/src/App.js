38bd8f0a898bc3f735c437e3010b268e91257c82
Daniel Ramsayer <13422934+danielramsayer@users.noreply.github.com>
2019-01-14 23:39:19 -0800

    Working!

--- random-programs/Random_React/my-new-app/src/App.js
+++ random-programs/Random_React/my-new-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-//import { CLIENTS } from './test_stuff/test_data';
-//import ReadDataIntoBrowser from './components/ReadDataIntoBrowser';
+import { CLIENTS } from './test_stuff/test_data';
+import ReadDataIntoBrowser from './components/ReadDataIntoBrowser';
 import Posts from './components/posts';
 import PostForm from './components/postForm';
 
@@ -8,26 +8,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //clients: CLIENTS,
+      clients: CLIENTS,
     };
   }
 
   render() {
-    /*const Tester = props => {
+    const Tester = props => {
       console.log(props.thing);
       return (<h1>{props.thing}</h1>);
-    }*/
+    }
     return (
       <div>
         <h1>Welcome to the Show</h1>
           <PostForm />
           <hr />
           <Posts />
-
+          <ReadDataIntoBrowser data={this.state.clients}/>
+          <Tester thing="This additional thing!"/>
       </div>
     );
   }
 }
-//<ReadDataIntoBrowser data={this.state.clients}/>
-//<Tester thing="This additional thing!"/>
+
 export default App;
