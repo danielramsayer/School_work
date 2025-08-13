5732c7180ce18a540c0ecd1bb55d21a9c81d9e2a
Daniel Ramsayer <daniel.ramsayer@gmail.com>
2025-08-13 15:52:49 -0700

    Created java file for class.

--- Coursera/Algorithms&Datastructures/Amazon/Module_1/Module1/src/App.java
+++ Coursera/Algorithms&Datastructures/Amazon/Module_1/Module1/src/App.java
@@ -1,9 +1,3 @@
-// public class App {
-//     public static void main(String[] args) throws Exception {
-//         System.out.println("Hello, World!");
-//     }
-// }
-
 public class App {
     public static void main(String[] args) {
         String[] daysOfWeek = new String[7];
@@ -11,10 +5,36 @@ public class App {
 
         System.out.println("First Element of Days: " + days[0]);
 
-        for(int i=0; i < days.length; i++) {
+        //Traversal
+        for (int i = 0; i < days.length; i++) {
             daysOfWeek[i] = days[i];
             System.out.println("Element: " + i + " " + daysOfWeek[i]);
         }
+        
+        //Insertion keep the size
+        daysOfWeek[3] = "Thursday";
+        for (int i = 0; i < daysOfWeek.length; i++) {
+            System.out.println("Element: " + i + " " + daysOfWeek[i]);
+        }
+
+        //Insertion change the size
+        String newElement = "Funday";
+        String[] tempArray = new String[8];
+        for (int index = 0; index < tempArray.length; index++) {
+            if (index == (tempArray.length - 1)) {
+                tempArray[index] = newElement; //insert new element at end
+            } else {
+                tempArray[index] = days[index]; //copy existing days
+            }
+        }
+
+        for (int i = 0; i < tempArray.length; i++) {
+            System.out.println("Element: " + i + " " + tempArray[i]);
+        }
+
+        //Deletion can be done by replacing an element with null or making a new array
+       
+
 
         // System.out.println("First Element of Days of Week: " + daysOfWeek[0]);
     }
